7a9cc16ce830612cec969a2df4f232e26cb4849e
Ed Rial <eddrial@hotmail.com>
2021-09-07 11:38:44 +0200

    recommit

---
+++ src/anapyse/anapyse.py
@@ -0,0 +1,8 @@
+'''
+Created on 7 Sep 2021
+
+@author: oqb
+'''
+
+if __name__ == '__main__':
+    pass
---
+++ src/anapyse/anapyse_class.py
@@ -0,0 +1,17 @@
+'''
+Created on 7 Sep 2021
+
+@author: oqb
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
